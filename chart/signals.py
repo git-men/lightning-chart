b784dc5ec35d18af625cbf76cf94041ff12e4f99
jeff <bbmyth@gmail.com>
2020-07-05 20:34:49 +0800

    图表使用缓存减少查询

--- chart/signals.py
+++ chart/signals.py
@@ -1,8 +1,17 @@
+import logging
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from api_basebone.signals import post_bsm_create
 from chart.models import Chart
+from django.core.cache import cache
+
+log = logging.getLogger(__name__)
+
+@receiver(post_save, sender=Chart, dispatch_uid='clean_chart_cache_by_save')
+def clean_chart_cache_by_save(sender, instance, **kwargs):
+    cache.delete(f'chart_config:{instance.id}')
+    log.debug('remove chart cache')
 
 
-# @receiver(post_bsm_create, sender=Chart, dispatch_uid='gen_chart')
-# def gen_chart(sender, instance: Chart, **kwargs):
-#     instance.gen_by_args()
+@receiver(post_delete, sender=Chart, dispatch_uid='clean_chart_cache_by_delete')
+def clean_chart_cache_by_delete(sender, instance, **kwargs):
+    cache.delete(f'chart_config:{instance.id}')
