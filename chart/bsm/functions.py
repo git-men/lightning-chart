f36b092f0feb69157df78f6d5349840482148ee4
kycool <kycoolcool@gmail.com>
2020-07-17 18:48:45 +0800

    添加数据，对象，用户，角色权限设置云函数

--- chart/bsm/functions.py
+++ chart/bsm/functions.py
@@ -1,11 +1,17 @@
 import logging
 
 from django.core.cache import cache
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
 
 from api_basebone.drf.response import success_response
 from api_basebone.restful.funcs import bsm_func
+from api_basebone.core import exceptions
 
 from chart.models import Chart
+from guardian.shortcuts import GroupObjectPermission, assign_perm
+from guardian.models import UserObjectPermission
 
 log = logging.getLogger(__name__)
 
@@ -68,3 +74,86 @@ def get_chart(user, id, **kwargs):
         filters=filters,
     )
     return success_response(data)
+
+
+@bsm_func(
+    login_required=True,
+    name='set_user_group_object_permission',
+    model=UserObjectPermission,
+)
+def set_user_group_object_permission(
+    user, data_id, codename, to_users=None, to_groups=None, **kwargs
+):
+    """
+    指定具体某一条数据给指定用户（组)对应的权限
+
+    Params:
+        id 数据 ID
+        codename string chart.view_chart
+        to_users list 指定给谁（用户 ID）
+        to_groups list 组id 列表
+    """
+    try:
+        app_label, perm_code = codename.split('.')
+        model_slug = perm_code.split('_')[1]
+        content_type = ContentType.objects.get(app_label=app_label, model=model_slug)
+    except Exception:
+        raise exceptions.BusinessException(
+            error_code=exceptions.PARAMETER_FORMAT_ERROR,
+            error_data=f'指定的 codename: {codename} 找不到对应的 ContentType',
+        )
+
+    try:
+        permission = Permission.objects.get(content_type=content_type, codename=perm_code)
+    except Exception:
+        raise exceptions.BusinessException(
+            error_code=exceptions.PARAMETER_FORMAT_ERROR,
+            error_data=f'指定的 codename: {codename} 找不到对应的 Permission',
+        )
+
+    try:
+        content_object = content_type.model_class().objects.get(id=data_id)
+    except Exception:
+        raise exceptions.BusinessException(
+            error_code=exceptions.PARAMETER_FORMAT_ERROR,
+            error_data=f'指定的 data_id: {data_id} 找不到对应的数据',
+        )
+
+    if not to_users and not to_groups:
+        raise exceptions.BusinessException(
+            error_code=exceptions.PARAMETER_FORMAT_ERROR, error_data='没有指定对应的用户和组',
+        )
+
+    if isinstance(to_users, list):
+        if to_users:
+            user_qs = get_user_model().objects.filter(id__in=to_users)
+            if user_qs:
+                UserObjectPermission.objects.filter(
+                    content_type=content_type,
+                    permission=permission,
+                    object_pk=str(content_object.id),
+                ).delete()
+                assign_perm(codename, user_qs, content_object)
+        else:
+            UserObjectPermission.objects.filter(
+                content_type=content_type,
+                permission=permission,
+                object_pk=str(content_object.id),
+            ).delete()
+
+    if isinstance(to_groups, list):
+        if to_groups:
+            group_qs = Group.objects.filter(id__in=to_groups)
+            if group_qs:
+                GroupObjectPermission.objects.filter(
+                    content_type=content_type,
+                    permission=permission,
+                    object_pk=str(content_object.id),
+                ).delete()
+                assign_perm(codename, group_qs, content_object)
+        else:
+            GroupObjectPermission.objects.filter(
+                content_type=content_type,
+                permission=permission,
+                object_pk=str(content_object.id),
+            ).delete()
